1b4ed852c121f297d9794c7b08d3ca4d9f38e25f
Mp-keyner <keyneroswaldo0@gmail.com>
2023-11-11 22:39:05 -0500

    11 de noviembre 10:39

--- src/index.js
+++ src/index.js
@@ -6,3 +6,16 @@ app.use(morgan("dev"));
 app.listen(3000, () => {
   console.log("listening on port 3000");
 });
+
+app.get("/usuarios", (req, res) => {
+  res.send([
+    {
+      nombre: "Juan Pérez",
+      apellido: "López",
+    },
+    {
+      nombre: "María Sánchez",
+      apellido: "García",
+    },
+  ]);
+});
